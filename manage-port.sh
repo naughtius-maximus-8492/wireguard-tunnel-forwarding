9f35143a571fb2c9610e183b6e31f22c75a530c7
naughtius-maximus-8492 <naughtius.maximus8492@gmail.com>
2025-05-25 13:34:40 +0100

    Formatting and logic fixes

--- manage-port.sh
+++ manage-port.sh
@@ -1,12 +1,17 @@
+
 source .env
 
 function print_help {
-	echo "Valid arguments:"
+	echo "### Help Menu ###"
 	echo "-h        ; Show this menu"
+	echo ""
+	echo "-- Port management --"
 	echo "-p <port> ; Set port to open/close"
 	echo "-c        ; Close the port rather than open it"
-	echo "-t        ; Makes the iptables rule temporary. Reboots will flush it"
 	echo "-u        ; Open a UDP port instead of TCP"
+	echo ""
+	echo "-- iptables management --"
+	echo "-t        ; Makes the iptables rule temporary. Reboots will flush it"
 	exit
 } 	
 
@@ -15,7 +20,9 @@ function show_ports {
 	iptables -S | grep dport | awk '{print $12,$14}'
 }
 
-help=false
+if [ "$#" -lt 1 ]; then
+	print_help
+fi
 
 # A : Adds rule
 # D : Deletes rule
@@ -50,13 +57,13 @@ fi
 set -o xtrace 
 
 # iptables PREROUTING rule
-iptables -t nat -$rule PREROUTING -i $PHYSICAL_INTERFACE -p $protocol --dport $port -j DNAT --to-destination $CLIENT_IP:$port
+iptables -t nat -$rule PREROUTING -i $PHYSICAL_INTERFACE -p $protocol --dport $port -j DNAT --to-destination 10.0.0.2:$port
 
 # Route packets client -> server
-iptables -$rule FORWARD -i $WG_INTERFACE -o $PHYSICAL_INTERFACE -p $protocol --sport $port -s $CLIENT_IP -j ACCEPT
+iptables -$rule FORWARD -i $WG_INTERFACE -o $PHYSICAL_INTERFACE -p $protocol --sport $port -s 10.0.0.2 -j ACCEPT
 
 # Route packets server -> client
-iptables -$rule FORWARD -i $PHYSICAL_INTERFACE -o $WG_INTERFACE -p $protocol --dport $port -d $CLIENT_IP -j ACCEPT
+iptables -$rule FORWARD -i $PHYSICAL_INTERFACE -o $WG_INTERFACE -p $protocol --dport $port -d 10.0.0.2 -j ACCEPT
 
 # Stop printing commands	
 set +o xtrace
--- server-install.sh
+++ server-install.sh
@@ -1,4 +1,9 @@
 source .env
+apt update
 apt install wireguard iptables iptables-persistent
+
+# Allow peer to access internet
 iptables -t nat -A POSTROUTING -o $PHYSICAL_INTERFACE -j MASQUERADE
+
+# Save rules for reboot
 iptables-save > /etc/iptables/rules.v4
