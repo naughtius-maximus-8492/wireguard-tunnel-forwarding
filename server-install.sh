98138a27eaac6558dd0cc4d9d3768d24bf395dcc
naughtius-maximus-8492 <naughtius.maximus8492@gmail.com>
2025-05-25 14:45:29 +0100

    Renamed as this is a universal script and added choice args for peer or server

--- server-install.sh
+++
@@ -1,9 +0,0 @@
-source .env
-apt update
-apt install wireguard iptables iptables-persistent
-
-# Allow peer to access internet
-iptables -t nat -A POSTROUTING -o $PHYSICAL_INTERFACE -j MASQUERADE
-
-# Save rules for reboot
-iptables-save > /etc/iptables/rules.v4
---
+++ tunnel-install.sh
@@ -0,0 +1,47 @@
+source .env
+
+apt update
+apt install wireguard
+
+function server_setup {
+	# Install iptables for routing and iptables-persistent to save them even after a reboot
+	apt install iptables iptables-persistent
+
+	# Allow peer to access internet
+	iptables -t nat -A POSTROUTING -o $PHYSICAL_INTERFACE -j MASQUERADE
+
+	# Save rules for reboot
+	iptables-save > /etc/iptables/rules.v4
+
+	# Use server configs
+#	cp wg-configs/wg-server.conf /etc/wireguard/wg0.conf
+
+	# Enable service to start now and at boot
+	systemctl --enable now wg-quick@wg0
+
+	exit
+}
+
+function peer_setup {
+	# Use client configs
+#	cp wg-configs/wg-peer.conf /etc/wireguard/wg0.conf
+
+	# Enable service to start now and at boot
+	systemctl --enable now wg-quick@wg0
+
+	exit
+}	
+
+
+while getopts hsp flag
+do
+	case "${flag}" in
+		s) server_setup ;;
+		p) peer_setup ;;
+	esac
+done
+
+echo "Valid arguments:"
+echo "-s	; Install server files"
+echo "-p	; Install peer files"
+exit
