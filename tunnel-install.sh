388f131920d69423cbe81d468e2e89f0425b3c1b
naughtius-maximus-8492 <naughtius.maximus8492@gmail.com>
2025-06-20 19:34:42 +0100

    Build command to paste onto client

--- tunnel-install.sh
+++ tunnel-install.sh
@@ -1,66 +1,51 @@
 source .env
 
-function server_setup {
-	echo "Installing wireguard with the server config..."
+echo "Installing wireguard server config..."
 
-	# Allow peer to access internet
-	iptables -t nat -A POSTROUTING -o $PHYSICAL_INTERFACE -j MASQUERADE
+# Allow peer to access internet
+iptables -t nat -A POSTROUTING -o $PHYSICAL_INTERFACE -j MASQUERADE
 
-	# Save rules for reboot
-	iptables-save > /etc/iptables/rules.v4
+# Save rules for reboot
+iptables-save > /etc/iptables/rules.v4
 
-	# Enable ipv4 forwarding
-	echo 1 > /proc/sys/net/ipv4/ip_forward
-	sysctl -p
+# Enable ipv4 forwarding
+echo 1 > /proc/sys/net/ipv4/ip_forward
+sysctl -p
 
-	# Generate server and peer keys
-	SERVER_PRIVATE_KEY=$(wg genkey)
-	SERVER_PUBLIC_KEY=$(echo $SERVER_PRIVATE_KEY | wg pubkey)
+# Generate server and peer keys
+SERVER_PRIVATE_KEY=$(wg genkey)
+SERVER_PUBLIC_KEY=$(echo $SERVER_PRIVATE_KEY | wg pubkey)
 
-	PEER_PRIVATE_KEY=$(wg genkey)
-	PEER_PUBLIC_KEY=$(echo $PEER_PRIVATE_KEY | wg pubkey)
+PEER_PRIVATE_KEY=$(wg genkey)
+PEER_PUBLIC_KEY=$(echo $PEER_PRIVATE_KEY | wg pubkey)
 
-	# Create server config
-	echo "[Interface]
-	Address = 10.0.0.1/24
-	ListenPort = 51820
-	PrivateKey = $SERVER_PRIVATE_KEY
+# Create server config
+echo "[Interface]
+Address = 10.0.0.1/24
+ListenPort = 51820
+PrivateKey = $SERVER_PRIVATE_KEY
 
-	[Peer]
-	PublicKey = $PEER_PUBLIC_KEY
-	AllowedIPs = 10.0.0.2/32
-	PersistentKeepalive = 25" > output.txt
-	
-	# Enable service to start now and at boot
-	systemctl enable --now wg-quick@wg0
-	systemctl status wg-quick@wg0
+[Peer]
+PublicKey = $PEER_PUBLIC_KEY
+AllowedIPs = 10.0.0.2/32
+PersistentKeepalive = 25" > output.txt
 
-	exit
-}
+# Enable service to start now and at boot
+systemctl enable --now wg-quick@wg0
+systemctl status wg-quick@wg0
 
-function peer_setup {
-	echo "Installing wireguard with the peer config..."
+# Build command to paste onto client
+echo "
+#################################################
+# PASTE THE BELOW COMMAND INTO YOUR CLIENT HOST #
+#################################################
 
-	# Use client configs
-	cp wg-configs/wg-peer.conf /etc/wireguard/wg0.conf
+echo \"[Interface]
+Address = 10.0.0.2/24
+PrivateKey = $PEER_PRIVATE_KEY
 
-	# Enable service to start now and at boot
-	systemctl enable --now wg-quick@wg0
-	systemctl status wg-quick@wg0
-
-	exit
-}	
-
-
-while getopts hsp flag
-do
-	case "${flag}" in
-		s) server_setup ;;
-		p) peer_setup ;;
-	esac
-done
-
-echo "Valid arguments:"
-echo "-s	; Install server files"
-echo "-p	; Install peer files"
-exit
+[Peer]
+PublicKey = $SERVER_PUBLIC_KEY
+Endpoint = $SERVER_PUBLIC_IP:$WG_PORT
+AllowedIPs = 0.0.0.0/0
+PersistentKeepalive = 25\" > /etc/wireguard/wg0.conf"
